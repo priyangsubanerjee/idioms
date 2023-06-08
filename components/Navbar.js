aa5243d6f9f45e93c3abfbd8fe296086b77e060a
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-08 13:56:55 +0530

    nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,17 +2,11 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="py-6 px-6 lg:px-20 sticky top-0 inset-x-0 bg-white/50 backdrop-blur">
+    <div className="py-6 px-6 lg:px-20">
       <h1 className="text-4xl lg:text-5xl font-serif">Idioms</h1>
-      <input
-        type="text"
-        className="bg-zinc-100 w-full h-12 px-5 rounded-lg mt-4"
-        placeholder="Search"
-        name=""
-        id=""
-      />
-      <div className="grid grid-cols-2 fixed bottom-0 inset-x-0 h-16 border-t">
-        <button className="w-full flex justify-center items-center border-r">
+
+      <div className="grid grid-cols-3 fixed bottom-0 inset-x-0 h-14 border-t">
+        <button className="w-full flex justify-center items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -28,7 +22,7 @@ function Navbar() {
             />
           </svg>
         </button>
-        <button className="w-full flex justify-center items-center">
+        <button className="w-full flex justify-center items-center border-x">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -41,6 +35,22 @@ function Navbar() {
               stroke-linecap="round"
               stroke-linejoin="round"
               d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+            />
+          </svg>
+        </button>
+        <button className="w-full flex justify-center items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
             />
           </svg>
         </button>

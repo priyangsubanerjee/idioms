ef3a7b2b969013512a25f9c5ce49956e112e7e61
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-09 00:35:36 +0530

    idioms

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,17 @@
 
 export default function Home() {
   return (
-    <main className="">
-      <div className="mt-5 px-6 flex items-center text-zinc-800">
-        <h1 className="text-3xl font-bold">Home</h1>
-        <div className="flex items-center space-x-10 ml-auto">
+    <main>
+      <div className="px-6 mt-16">
+        <h1 className="font-serif font-semibold text-4xl">Idioms</h1>
+        <div className="px-5 bg-zinc-100 flex items-center w-full h-12 mt-5 rounded-md">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="2"
+            stroke-width="1.5"
             stroke="currentColor"
-            class="h-6 w-6"
+            class="w-5 h-5 text-zinc-500 mr-2"
           >
             <path
               stroke-linecap="round"
@@ -20,78 +20,90 @@ export default function Home() {
               d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
             />
           </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            stroke="currentColor"
-            class="h-6 w-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 4.5v15m7.5-7.5h-15"
-            />
-          </svg>
+
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-transparent"
+            name=""
+            id=""
+          />
         </div>
+        <ul className="mt-7 text-sm">
+          <li className="flex items-center space-x-2 text-blue-600 border-b py-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+              />
+            </svg>
+
+            <span>Random</span>
+          </li>
+          <li className="flex items-center space-x-2 text-blue-600 border-b py-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+              />
+            </svg>
+
+            <span>Bookmarked</span>
+          </li>
+          <li className="flex items-center space-x-2 text-blue-600 border-b py-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
+              />
+            </svg>
+
+            <span>Trending</span>
+          </li>
+          <li className="flex items-center space-x-2 text-blue-600 py-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+
+            <span>Contribute</span>
+          </li>
+        </ul>
       </div>
-      <nav className="fixed bottom-0 inset-x-0 w-full py-5 bg-zinc-50/50 border-t grid grid-cols-3">
-        <button className="flex items-center space-x-2 w-full justify-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="h-5 w-5"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-            />
-          </svg>
-
-          <span className="text-sm">Home</span>
-        </button>
-        <button className="flex opacity-60 items-center space-x-2 w-full justify-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="h-5 w-5"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
-            />
-          </svg>
-
-          <span className="text-sm">Random</span>
-        </button>
-        <button className="flex opacity-60 items-center space-x-2 w-full justify-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="h-5 w-5"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
-            />
-          </svg>
-
-          <span className="text-sm">Saved</span>
-        </button>
-      </nav>
     </main>
   );
 }

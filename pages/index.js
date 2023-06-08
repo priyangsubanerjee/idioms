eb2dbbbdc922549c1679a63eafccd8d8ecf547db
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-09 00:44:06 +0530

    bg

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
 
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <main>
-      <div className="px-6 mt-16">
+    <main className="h-screen bg-zinc-950 text-white pt-16">
+      <Head>
+        <meta name="theme-color" content="rgb(9 9 11)" />
+      </Head>
+      <div className="px-6">
         <h1 className="font-serif font-semibold text-4xl">Idioms</h1>
-        <div className="px-5 bg-zinc-100 flex items-center w-full h-12 mt-5 rounded-md">
+        <div className="px-5 bg-zinc-900 flex items-center w-full h-12 mt-5 rounded-md">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -30,7 +35,7 @@ export default function Home() {
           />
         </div>
         <ul className="mt-7 text-sm">
-          <li className="flex items-center space-x-2 text-blue-600 border-b py-3">
+          <li className="flex items-center space-x-2 text-blue-400 border-b border-zinc-800 py-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -48,7 +53,7 @@ export default function Home() {
 
             <span>Random</span>
           </li>
-          <li className="flex items-center space-x-2 text-blue-600 border-b py-3">
+          <li className="flex items-center space-x-2 text-blue-400 border-b border-zinc-800 py-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -66,7 +71,7 @@ export default function Home() {
 
             <span>Bookmarked</span>
           </li>
-          <li className="flex items-center space-x-2 text-blue-600 border-b py-3">
+          <li className="flex items-center space-x-2 text-blue-400 border-b  border-zinc-800 py-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -84,7 +89,7 @@ export default function Home() {
 
             <span>Trending</span>
           </li>
-          <li className="flex items-center space-x-2 text-blue-600 border-b py-3">
+          <li className="flex items-center space-x-2 text-blue-400 border-b  border-zinc-800 py-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -102,7 +107,7 @@ export default function Home() {
 
             <span>Sentiment</span>
           </li>
-          <li className="flex items-center space-x-2 text-blue-600 py-4">
+          <li className="flex items-center space-x-2 text-blue-400 py-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -122,7 +127,7 @@ export default function Home() {
           </li>
         </ul>
 
-        <div className="bg-zinc-50 p-4 mt-10">
+        <div className="bg-zinc-900 p-4 mt-10">
           <h2 className="font-medium text-sm">Rules for contributing</h2>
           <p className="text-xs text-zinc-500 mt-1 leading-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.

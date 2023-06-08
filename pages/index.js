e0680192ba15cc181e9c05093df6138391eac28c
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-08 22:18:44 +0530

    index

--- pages/index.js
+++ pages/index.js
@@ -7,81 +7,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="fixed inset-0 h-full w-full bg-white overflow-y-auto">
-      <Head>
-        <meta name="theme-color" content="rgb(238 242 255)" />
-      </Head>
-      <div>
-        <div className="bg-indigo-50 py-16">
-          <img src="/loginSketch.svg" className="h-32 mx-auto" alt="" />
-        </div>
-        <div className="px-4 mt-10 text-center">
-          <h1 className="text-3xl font-serif font-medium text-center">
-            Learn Idioms
-          </h1>
-          <p className="mt-4 leading-7 text-zinc-700 text-xs">
-            The more words you know, the more clearly and powerfully you will
-            think and the more ideas you will invite into your mind
-          </p>
-
-          <div className="flex items-center justify-center space-x-3 text-indigo-600 mt-9">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="w-4 h-4"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                clip-rule="evenodd"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="w-4 h-4"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                clip-rule="evenodd"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="w-4 h-4"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                clip-rule="evenodd"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="w-4 h-4"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </div>
-
-          <div className="px-6 mt-10">
-            <button className="w-full bg-zinc-100 rounded-md text-zinc-900 h-12 flex items-center justify-center">
-              <span>Sign in with Google</span>
-            </button>
-          </div>
-        </div>
-      </div>
+    <main className="fixed inset-0 h-full w-full flex flex-col bg-white overflow-y-auto snap-y snap-mandatory snap-always">
+      <div className="h-full w-full shrink-0 bg-red-100 snap-start"></div>
+      <div className="h-full w-full shrink-0 bg-blue-100 snap-start"></div>
+      <div className="h-full w-full shrink-0 bg-green-100 snap-start"></div>
     </main>
   );
 }

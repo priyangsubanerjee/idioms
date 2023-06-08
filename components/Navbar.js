07cc4a0ab83b2d3b5aad1d2bfd1144bdbd01ee8a
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-08 09:06:40 +0530

    head

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="h-28 lg:mt-10 px-6 lg:px-20 flex items-center">
+    <div id="nav" className="pt-6 px-6 lg:px-20 flex items-center">
       <h1 className="font-serif text-4xl lg:text-5xl font-semibold">Idioms</h1>
     </div>
   );

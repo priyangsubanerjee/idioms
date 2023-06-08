88946ad274430cedd0bdf6d2c6b0e024bd8d5120
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-08 09:17:53 +0530

    mobile device

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <div className="hidden bg-white fixed inset-0 h-full w-full md:flex flex-col items-center justify-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-10 h-10"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+          />
+        </svg>
+        <span className="text-sm text-zinc-700 mt-10 bg-zinc-50 p-2 rounded">
+          This app is suported only on mobile devices
+        </span>
+      </div>
+    </div>
+  );
 }

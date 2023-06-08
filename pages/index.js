dffbc3ed3455cdb69defe9eba70e7eb41a7faead
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-08 23:31:57 +0530

    icons

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,33 @@ export default function Home() {
     <main className="">
       <div className="mt-7 px-6 flex items-center">
         <h1 className="text-3xl font-bold">Home</h1>
-        <div className="flex items-center space-x-5 ml-auto">
+        <div className="flex items-center space-x-10 ml-auto">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            class="h-5 w-5"
           >
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
               d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-5 w-5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
             />
           </svg>
         </div>
@@ -30,7 +44,7 @@ export default function Home() {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            class="h-5 w-5"
           >
             <path
               stroke-linecap="round"
@@ -48,7 +62,7 @@ export default function Home() {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            class="h-5 w-5"
           >
             <path
               stroke-linecap="round"
@@ -66,7 +80,7 @@ export default function Home() {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            class="h-5 w-5"
           >
             <path
               stroke-linecap="round"

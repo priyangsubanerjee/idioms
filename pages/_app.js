ed2d48e3323502eb7df4c5345d1affd8d41f7f8b
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-08 14:10:03 +0530

    home

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }) {
   return (
     <div>
-      <Navbar />
       <Component {...pageProps} />
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import { useLayoutEffect } from "react";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   useLayoutEffect(() => {}, []);
   return (
-    <main className="px-6 space-y-3 pb-20">
-      <div className="bg-zinc-100 h-44 w-full"></div>
-      <div className="bg-zinc-100 h-44 w-full"></div>
-      <div className="bg-zinc-100 h-44 w-full"></div>
+    <main className="bg-gradient-to-b from-blue-900 to-sky-600 h-screen">
+      <Head>
+        <meta name="theme-color" content="rgb(30 58 138)" />
+      </Head>
     </main>
   );
 }

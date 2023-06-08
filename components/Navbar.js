12df6c16b5dd84022e2ab9c6994d9d779b837814
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-08 14:00:04 +0530

    menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,10 +2,43 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="py-6 px-6 lg:px-20">
-      <h1 className="text-4xl lg:text-5xl font-serif">Idioms</h1>
+    <div className="py-5 px-6 lg:px-20">
+      <div className="flex items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
 
-      <div className="grid grid-cols-3 fixed bottom-0 inset-x-0 h-14 border-t">
+        <h1 className="text-3xl ml-3 lg:text-5xl font-serif">Idioms</h1>
+        <button className="ml-auto">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-5 h-5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <div className="grid grid-cols-3 fixed bottom-0 inset-x-0 h-14 shadow-2xl">
         <button className="w-full flex justify-center items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -22,7 +55,7 @@ function Navbar() {
             />
           </svg>
         </button>
-        <button className="w-full flex justify-center items-center border-x">
+        <button className="w-full flex justify-center items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
